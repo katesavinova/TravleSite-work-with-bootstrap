fad973b7ff47f92ef0e287ff51d9fcf466f07876
katesavinova <63250525+katesavinova@users.noreply.github.com>
2020-06-18 12:39:22 +0400

    Add files

---
+++ www/index.php
@@ -0,0 +1,43 @@
+<?header('Content-Type: text/html; charset= utf-8');?>
+<!DOCTYPE html>
+<html lang="rus">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>ITravel</title>
+</head>
+<body>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal">ITravel</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Sign up</a>
+    </div>
+    <div class="d-flex flex-wrap">
+<? for($i=0; $i<6; $i++): ?>
+    <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Предложение <?echo ($i+1);?></h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title"><?echo rand(150,500)."$";?> <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+<? endfor; ?>
+</div>
+</body>
+</html>
